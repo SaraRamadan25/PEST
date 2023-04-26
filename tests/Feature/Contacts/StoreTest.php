87d1551920dbb399dbc1ded1580b7a8412ef8f6e
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-04-26 15:27:43 +0200

    before creating DataSets

--- tests/Feature/Contacts/StoreTest.php
+++ tests/Feature/Contacts/StoreTest.php
@@ -4,9 +4,11 @@ use function Pest\Faker\faker;
 
 // when we use the above plugin we don't need to use trait ( WithFaker )
 
-it('can store a contact' ,function(){
+// if we want to pass a parameter to our test closure, we can use datasets at the end of our code
 
-    login()->post('/contacts',[
+it('can store a contact' ,function(array $data){
+
+    login()->post('/contacts', [... [
         'first_name' => faker()->firstName,
         'last_name' => faker()->lastName,
         'email' => faker()->email,
@@ -16,7 +18,7 @@ it('can store a contact' ,function(){
         'region' => 'Derbyshire',
         'country' => faker()->randomElement(['us', 'ca']),
         'postal_code' => faker()->postcode,
-    ])
+    ], ... $data])
     ->assertRedirect('/contacts')
         ->assertSessionHas('success', 'Contact created.');
 
@@ -28,4 +30,10 @@ it('can store a contact' ,function(){
         ->phone->toBePhoneNumber
         ->region->toBe('Derbyshire')
         ->country->toBeIn(['us', 'ca']);
-});
+})->with([
+    'generic'=>[[]],
+    'email with spaces'=> [['email'=>'"luke downing"@downing.tech']],
+    'co.uk with sara'=>[['email'=>'info@me.co.uk','first_name'=>'Sara']],
+    'post code with 25 chars'=>[['postal_code'=>\Illuminate\Support\Str::repeat('a',25)]]
+
+]);
