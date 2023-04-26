7720f7bf75deafdb025c883b552927d70788439b
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-04-26 15:35:31 +0200

    Create Shared Datasets - Emails

---
+++ tests/Datasets/Emails.php
@@ -0,0 +1,10 @@
+<?php
+
+dataset('Valid Emails', function () {
+    return [
+        'foo@bar.com',
+       '"luke downing"@downing.tech',
+        'info@me.co.uk',
+        'arentdatasetcool@pestphp.com',
+    ];
+});
